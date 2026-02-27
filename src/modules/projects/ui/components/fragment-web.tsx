23008783a3ee7cc639fb17fad82f5d9dff4e04a2
zaid-nawaz <zaidnawaz2005@gmail.com>
2026-02-27 16:24:55 +0530

    fragment view

---
+++ src/modules/projects/ui/components/fragment-web.tsx
@@ -0,0 +1,70 @@
+import { useState } from "react";
+import { ExternalLinkIcon, RefreshCcwIcon } from "lucide-react";
+
+import { Hint } from "@/components/hint";
+import { Fragment } from "@/generated/prisma/client";
+import { Button } from "@/components/ui/button";
+
+interface Props {
+  data: Fragment;
+};
+
+export function FragmentWeb({ data }: Props) {
+  const [copied, setCopied] = useState(false);
+  const [fragmentKey, setFragmentKey] = useState(0);
+
+  const onRefresh = () => {
+    setFragmentKey((prev) => prev + 1);
+  };
+
+  const handleCopy = () => {
+    navigator.clipboard.writeText(data.sandboxUrl);
+    setCopied(true);
+    setTimeout(() => setCopied(false), 2000);
+  };
+
+  return (
+    <div className="flex flex-col w-full h-full">
+      <div className="p-2 border-b bg-sidebar flex items-center gap-x-2">
+        <Hint text="Refresh" side="bottom" align="start">
+          <Button size="sm" variant="outline" onClick={onRefresh}>
+            <RefreshCcwIcon />
+          </Button>
+        </Hint>
+        <Hint text="Click to copy" side="bottom">
+          <Button 
+            size="sm" 
+            variant="outline" 
+            onClick={handleCopy}
+            disabled={!data.sandboxUrl || copied}
+            className="flex-1 justify-start text-start font-normal"
+          >
+            <span className="truncate">
+              {data.sandboxUrl}
+            </span>
+          </Button>
+        </Hint>
+        <Hint text="Open in a new tab" side="bottom" align="start">
+          <Button
+            size="sm"
+            disabled={!data.sandboxUrl}
+            variant="outline"
+            onClick={() => {
+              if (!data.sandboxUrl) return;
+              window.open(data.sandboxUrl, "_blank");
+            }}
+          >
+            <ExternalLinkIcon />
+          </Button>
+        </Hint>
+      </div>
+      <iframe
+        key={fragmentKey}
+        className="h-full w-full"
+        sandbox="allow-forms allow-scripts allow-same-origin"
+        loading="lazy"
+        src={data.sandboxUrl}
+      />
+    </div>
+  )
+};
--- src/modules/projects/ui/views/project-view.tsx
+++ src/modules/projects/ui/views/project-view.tsx
@@ -7,6 +7,7 @@ import { MessagesContainer } from "../components/messages-container";
 import { Suspense, useState } from "react";
 import { Fragment } from "@/generated/prisma/client";
 import { ProjectHeader } from "../components/project-header";
+import { FragmentWeb } from "../components/fragment-web";
 
 
 
@@ -43,7 +44,7 @@ export const ProjectView = ({projectId } : Props) => {
                 minSize={50}
                 >
 
-            TODO : PREVIEW
+        {!!activeFragment && <FragmentWeb data={activeFragment} />}
                 </ResizablePanel>
             </ResizablePanelGroup>
         </div>
